9703aaa93fcdbbaaea92414115d40467cf1c4267
Sarah Bondurant <sebondur@uark.edu>
2019-11-12 22:36:02 -0600

    Fixed library fragment so it mimics the other fragments

---
+++ app/src/main/java/com/example/loggo/ui/library/LibraryViewModel.java
@@ -0,0 +1,18 @@
+package com.example.loggo.ui.library;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class LibraryViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public LibraryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is library fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
