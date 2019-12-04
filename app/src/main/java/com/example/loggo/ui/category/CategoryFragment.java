db046fa8e3dec328cbf70d6a72016a21159cc42d
Sarah Bondurant <sebondur@uark.edu>
2019-12-03 19:50:04 -0600

    Preliminary linking of category fragment to library fragment

--- app/src/main/java/com/example/loggo/ui/category/CategoryFragment.java
+++ app/src/main/java/com/example/loggo/ui/category/CategoryFragment.java
@@ -11,10 +11,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.loggo.R;
 
-public class CategoryFragment extends Fragment {
+public class CategoryFragment extends Fragment implements View.OnClickListener {
 
     private CategoryViewModel categoryViewModel;
 
@@ -28,11 +29,28 @@ public class CategoryFragment extends Fragment {
         categoryViewModel =
                 ViewModelProviders.of(this).get(CategoryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_category, container, false);
+
+        // Set OnClick Listener for all the buttons
+        root.findViewById(R.id.button_tv).setOnClickListener(this);
+
         return root;
+    }
+
+    public void onClick(View v) {
+        // Set Backlog to be loaded to button pressed
+        switch (v.getId()) {
+            // TODO: all the buttons
+            case R.id.button_tv:
+                // TODO
+                break;
+        }
+
+        // Navigate to library fragment
+        NavHostFragment.findNavController(this).navigate(R.id.action_navigation_category_to_navigation_library);
     }
 }
 
-/* TODO: button listener
+/* TODO: button listener & navcontroller
 *        sends user to library fragment
 *        sends button selected data
 *        get rid of back button?
